ccc5be190eb1f9030380e833ef18f9165e7b4c83
Vuong Minh <vuongxuongminh@gmail.com>
2019-05-21 23:27:19 +0700

    Added exceptions

---
+++ src/Exceptions/LandLineVNException.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @link https://github.com/phpviet/validation
+ * @copyright (c) PHP Viet
+ * @license [MIT](https://opensource.org/licenses/MIT)
+ */
+
+namespace PHPViet\Validation\Exceptions;
+
+use Respect\Validation\Exceptions\ValidationException;
+
+/**
+ * @author Vuong Minh <vuongxuongminh@gmail.com>
+ * @since 1.0.0
+ */
+class LandLineVNException extends ValidationException
+{
+
+    public static $defaultTemplates = [
+        self::MODE_DEFAULT => [
+            self::STANDARD => '{{name}} must be a valid Viet Nam land line phone number',
+        ],
+        self::MODE_NEGATIVE => [
+            self::STANDARD => '{{name}} must be a valid Viet Nam land line phone number',
+        ],
+    ];
+
+}
